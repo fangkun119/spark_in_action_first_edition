2f49e089a54bc535df53a2c1d18a5e9a023d3f21
fangkun119 <fangkun119@hotmail.com>
2020-01-11 05:38:57 -0800

    ch02 ch03

--- ch03/scala/org/sia/chapter03App/App.scala
+++ ch03/scala/org/sia/chapter03App/App.scala
@@ -4,7 +4,6 @@ import org.apache.spark.sql.SparkSession
 import scala.io.Source.fromFile
 
 object App {
-
   def main(args : Array[String]) {
     // TODO expose appName and master as app. params
     val spark = SparkSession.builder()
@@ -16,22 +15,33 @@ object App {
 
     // TODO expose inputPath as app. param
     val homeDir = System.getenv("HOME")
-    val inputPath = homeDir + "/sia/github-archive/2015-03-01-0.json"
+    val inputPath = homeDir + "/sia/github-archive/2015-03-01-0.json" //single file
+    // val inputPath = homeDir + "/sia/github-archive/*.json" //multiple files 
     val ghLog = spark.read.json(inputPath)
 
     val pushes = ghLog.filter("type = 'PushEvent'")
-    val grouped = pushes.groupBy("actor.login").count
-    val ordered = grouped.orderBy(grouped("count").desc)
+    // pushes.printSchema  						    // display infered schema 
+	// println("all events: " + ghLog.count) 		// total log count
+	// println("only pushes: " + pushes.count)		// log count after filtering
+	// pushes.show(5)								// first 5 lines of the filtered log
 
-      // TODO expose empPath as app. param
+    val grouped = pushes.groupBy("actor.login").count 
+	// grouped.show(5) // group by user name
+
+    val ordered = grouped.orderBy(grouped("count").desc)
+    // ordered.show(5) // top 5 users with the push count
+
+    // TODO expose empPath as app. param
     val empPath = homeDir + "/first-edition/ch03/ghEmployees.txt"
     val employees = Set() ++ (
-      for {
-        line <- fromFile(empPath).getLines
+      // for expression: http://mng.bz/k8q2
+      for { 
+        line <- fromFile(empPath).getLines  
       } yield line.trim
     )
-    val bcEmployees = sc.broadcast(employees) #A Broadcast the             employees set
 
+    //Broadcast the employees set to all nodes
+    val bcEmployees = sc.broadcast(employees)  
 
     import sqlContextspark.implicits._
     val isEmp = user => bcEmployees.value.contains(user)
--- ch03/scala/org/sia/chapter03App/GitHubDay.scala
+++ ch03/scala/org/sia/chapter03App/GitHubDay.scala
@@ -3,12 +3,22 @@ package org.sia.chapter03App
 import scala.io.Source.fromFile
 import org.apache.spark.sql.SparkSession
 
-object GitHubDay {
+// object: http://mng.bz/y2ja
+// is Spark Singleton
+// must be consistent with the file name
+object GitHubDay { 
   def main(args : Array[String]) {
+    // SparkSession doc: http://mng.bz/j9As
+    ////// do not get appName and master from config  //////
+    ////// spark-submit will provide them //////////////////
+    // val spark = SparkSession.builder()
+    //    .appName("GitHub push counter")
+    //    .master("local[*]")
+    //    .getOrCreate()
     val spark = SparkSession.builder().getOrCreate()
+    val sc    = spark.sparkContext
 
-    val sc = spark.sparkContext
-
+    // 
     val ghLog = spark.read.json(args(0))
 
     val pushes = ghLog.filter("type = 'PushEvent'")
@@ -28,6 +38,8 @@ object GitHubDay {
     val sqlFunc = spark.udf.register("SetContainsUdf", isEmp)
     val filtered = ordered.filter(sqlFunc($"login"))
 
+    ////// output destination and format is depend on `spark-submit` //////
+    // filtered.show()
     filtered.write.format(args(3)).save(args(2))
   }
 }
